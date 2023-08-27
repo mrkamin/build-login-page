f3536ab58fb1cff85fa586d819446974954974ed
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-08-27 20:30:41 +0500

    Add Validation to the Friends

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,4 +1,11 @@
 # Represents a friend in the application.
 class Friend < ApplicationRecord
   belongs_to :user
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :phone, presence: true
+  validates :twitter, presence: true, uniqueness: true
+  
 end
